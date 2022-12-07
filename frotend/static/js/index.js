0bbbb6153162cbaac7f8ee3ff9cc87f75fbf3987
jhonatas <jhonatas.flor@dcx.ufpb.br>
2022-12-06 21:27:23 -0300

    Desativar cors no backend

--- frotend/static/js/index.js
+++ frotend/static/js/index.js
@@ -1,8 +1,47 @@
-import { Connect } from "./Connect";
-import { DataHandler } from "./DataHandle";
+class DataHandler{
+    eventos;
+    palestrantes;
+    eventosDestaque;
+    constructor(eventos,palestrantes){
+        this.eventos = eventos;
+        this.palestrantes = palestrantes;
+    }
+}
+
+class Connect{
+    serverAdd ;
+    constructor(serverAdd){
+        this.serverAdd = serverAdd
+    }
+
+    async postRequest(route, body){
+        const config = {
+            method:"POST",
+            body: JSON.stringify(body),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        }
+        const URL = `${this.serverAdd}/${route}`
+        const response = await fetch(URL,config);
+        if(!response.ok) return {MSG:"Fetch error"}
+        return await response.json();
+    }
+
+    async getRequest(route){
+        const config = {method: "GET"}
+        const URL = `${this.serverAdd}/${route}`
+        const response = await fetch(URL,config);
+        if(!response.ok) return {MSG:"Fetch error"};
+        console.log(response.json())
+        return await response.json();
+    }
+
+    async request({method,route, body}){
+        return method === "GET" ? await this.getRequest(route) : await this.postRequest(route,body);
+    }
+}
 
 const dataHandler = null;
-const connect = new Connect("localhost:3000");
+const connect = new Connect("http://127.0.0.1:3000");
 const contador = 0;
 const activeSlider = false;
 const imgSlider = document.getElementById("ImgSlider");
@@ -14,8 +53,7 @@ function setSlide(){
     txtSlider.src = imgPath[contador];
 }
 window.addEventListener('DOMContentLoaded', async i => {
-    console.log(await connect.request("GET", "palestrantes"))
-    dataHandler = new DataHandler(await connect.request("GET", "palestrantes"), await connect.request("GET", "eventos"));
+    dataHandler = new DataHandler(await connect.request({method:"GET", route:"palestrantes", body:{}}), await connect.request({method:"GET", route:"eventos", body:{}}));
     imgSlider.src = imgPath[contador];
     txtSlider.src = titulos[contador];
     activeSlider = true;
