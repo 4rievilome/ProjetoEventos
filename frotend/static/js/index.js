bade6f2a6963478327e3bbb9a4016e670534875d
jhonatas <jhonatas.flor@dcx.ufpb.br>
2022-12-09 00:22:47 -0300

    Colocando o front em classes

--- frotend/static/js/index.js
+++ frotend/static/js/index.js
@@ -1,9 +1,55 @@
 class DataHandler {
   eventos;
   palestrantes;
-  constructor(eventos, palestrantes) {
+  itens;
+  constructor(eventos, palestrantes, itens) {
     this.eventos = eventos;
     this.palestrantes = palestrantes;
+    this.itens = itens;
+  }
+
+  getEventos(){
+    return this.eventos;
+  }
+
+  getPalestrantes(){
+    return this.palestrantes;
+  }
+
+  getItens(){
+    return this.itens;
+  }
+
+  setEventos(eventos){
+    this.eventos = eventos
+  }
+
+  setPalestrantes(palestrantes){
+    this.palestrantes = palestrantes
+  }
+
+  setItens(itens){
+    this.itens = itens;
+  }
+
+  numEventos(){
+    return this.eventos.length
+  }
+
+  numPalestrantes(){
+    return this.palestrantes.length
+  }
+
+  numItens(){
+    return this.itens.length
+  }
+  
+  precoTotal(listaItens){
+    let valorTotal = 0;
+    for(item of listaItens){
+      valorTotal+=item?.preco;
+    }
+    return valorTotal;
   }
 }
 
@@ -40,55 +86,94 @@ class Connect {
   }
 }
 
-let dataHandler = null;
-const connect = new Connect('http://127.0.0.1:3000');
-let contador = 0;
-let imgSlider = null;
-let txtSlider = null;
-let bullets = null;
-const imgPath = [
-  'static/img/dragao.png',
-  'static/img/e1.webp',
-  'static/img/BGS.jpeg',
-];
-const titulos = [
-  'OFICINA DE CRIAÇÃO DE GAMES',
-  'PALESTRA SOBRE IOT',
-  'BAIANINHO DE MAUÁ ARRASANDO',
-];
+class SistemaEvento{
+  contador = 0;
+  imgPath = [
+    'static/img/dragao.png',
+    'static/img/e1.webp',
+    'static/img/BGS.jpeg',
+  ];
+  titulos = [
+    'OFICINA DE CRIAÇÃO DE GAMES',
+    'PALESTRA SOBRE IOT',
+    'BAIANINHO DE MAUÁ ARRASANDO',
+  ];
 
-function setSlide() {
-  imgSlider.src = imgPath[contador];
-  txtSlider.innerText = titulos[contador];
-  for (e of bullets) {
-    e.style.backgroundColor = 'transparent';
+  imgSlider = null;
+  txtSlider = null;
+  bullets = null;
+  dataHandler = null;
+  connect = null;
+  constructor(dataHandler, connect){
+    this.dataHandler = dataHandler;
+    this.connect = connect;
+    this.imgSlider = imgSlider;
+    this.imgSlider.src = this.imgPath[this.contador];
+    this.txtSlider = txtSlider;
+    this.txtSlider.innerText = this.titulos[this.contador];
+    this.bullets = bullets;
+  };
+
+  mudaPosicao(){
+    for(e of this.bullets){
+      e.style.backgroundColor = 'transparent';
+    }
+    this.bullets[this.contador].style.backgroundColor = 'white';
   }
-  bullets[contador].style.backgroundColor = 'white';
+  mudaSlide(){
+    this.imgSlider.src = this.imgPath[this.contador];
+    this.txtSlider.innerText = this.titulos[this.contador];
+    this.mudaPosicao();
+  }
+
+  incrementaContador(){
+    this.contador+=1;
+  }
+
+  getContador(){
+    return this.contador;
+  }
+  setContador(id){
+    this.contador = id;
+  }
 }
 
+const connect = new Connect('http://127.0.0.1:3000');
+let sisEvento = null;
+
 window.addEventListener('DOMContentLoaded', async () => {
-  imgSlider = document.getElementById('ImgSlider');
-  txtSlider = document.getElementById('TxtSlider');
-  bullets = document.getElementsByClassName('sliderCounter');
-  dataHandler = new DataHandler(
-    await connect.request({ method: 'GET', route: 'palestrantes', body: {} }),
-    await connect.request({ method: 'GET', route: 'eventos', body: {} }),
-  );
-  console.log(dataHandler)
-  imgSlider.src = imgPath[contador];
-  txtSlider.innerText = titulos[contador];
+  let dataHandler
+  try{
+    dataHandler = new DataHandler(
+      await connect.request({ method: 'GET', route: 'palestrantes', body: {} }),
+      await connect.request({ method: 'GET', route: 'eventos', body: {} }),
+    );
+  }catch(e){
+    console.error("Error %e", e)
+  }
+  
+  try{
+    sisEvento = new SistemaEvento(dataHandler, 
+      connect,
+      document.getElementById('ImgSlider'), 
+      document.getElementById('TxtSlider'), 
+      document.getElementsByClassName('sliderCounter') 
+    )
+  }catch(e){
+    console.error("error %e", e)
+  }
+  
+  console.log("load")
 });
 
-function getClick(element, id) {
-  for (e of bullets) {
-    e.style.backgroundColor = 'transparent';
-  }
-  element.style.backgroundColor = 'white';
-  contador = id;
-  setSlide();
+function getClick(id){
+    sisEvento.setContador(id);
+    sisEvento.mudaSlide()
 }
 
 setInterval(() => {
-  contador < 2 ? contador++ : (contador = 0);
-  setSlide();
+  if(sisEvento){
+    sisEvento.getContador() < 2 ? sisEvento.incrementaContador() : sisEvento.setContador(0);
+    sisEvento.mudaSlide();
+  }
 }, 2500);
